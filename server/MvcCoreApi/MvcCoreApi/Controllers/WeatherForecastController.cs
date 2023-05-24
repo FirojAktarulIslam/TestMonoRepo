f39ee23bfce1ce1514ec1af2fd6f7a7cab4e0336
Firoj <FIslam@evolenthealth.com>
2023-05-24 14:36:33 -0400

    Update WeatherForecastController.cs

--- server/MvcCoreApi/MvcCoreApi/Controllers/WeatherForecastController.cs
+++ server/MvcCoreApi/MvcCoreApi/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace MvcCoreApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        //test summary
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
